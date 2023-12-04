0eff3eedebf3223f29f6845f7a6a57b7f263c551
kovrizhka <pivsru@gmail.com>
2023-12-04 22:15:42 +0500

    picScanner v0.1 :)

--- src/main/java/com/example/axalittest/HelloApplication.java
+++ src/main/java/com/example/axalittest/HelloApplication.java
@@ -1,13 +1,9 @@
 package com.example.axalittest;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     public static void main(String[] args) {
